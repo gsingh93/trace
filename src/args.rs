b986775d5885d297f0d030ed137c76817ee8e64e
hcpl <hcpl.prog@gmail.com>
2018-11-05 04:58:09 +0200

    Switch to `proc-macro`
    As of the latest stable Rust 1.30, there is no need to depend on the
    unstable plugin interface to accomplish goals of this crate.
    Consequently, the requirement to chase the latest nightly has also been
    obsoleted.
    
    List of functionality changes:
    * Use `extern crate trace;` + `use trace::trace;` instead of
      `#![feature(custom_attribute, plugin)]` + `![plugin(trace)]`.
    * Using `#![trace]` (as an inner attribute) is not stable yet ---
      `example_mod` has been changed to use `#[trace]` as an outer attribute
      instead.
    * Traces for functions and methods now print "arg_name = value" instead
      of "arg_name: value" for arguments (in my opinion, they look more
      readable).

---
+++ src/args.rs
@@ -0,0 +1,247 @@
+use std::collections::HashSet;
+
+use syn::{
+    self,
+    spanned::Spanned,
+};
+
+
+pub(crate) struct Args {
+    pub(crate) prefix_enter: String,
+    pub(crate) prefix_exit: String,
+    pub(crate) filter: Filter,
+    pub(crate) pause: bool,
+}
+
+pub(crate) enum Filter {
+    None,
+    Enable(HashSet<proc_macro2::Ident>),
+    Disable(HashSet<proc_macro2::Ident>),
+}
+
+const DEFAULT_PREFIX_ENTER: &str = "[+]";
+const DEFAULT_PREFIX_EXIT: &str = "[-]";
+const DEFAULT_PAUSE: bool = false;
+
+impl Args {
+    pub(crate) fn from_raw_args(raw_args: syn::AttributeArgs) -> Result<Self, Vec<syn::parse::Error>> {
+        // Different types of arguments accepted by `#[trace]`;
+        // spans are needed for friendly error reporting of duplicate arguments
+        enum Arg {
+            PrefixEnter(proc_macro2::Span, String),
+            PrefixExit(proc_macro2::Span, String),
+            Enable(proc_macro2::Span, HashSet<proc_macro2::Ident>),
+            Disable(proc_macro2::Span, HashSet<proc_macro2::Ident>),
+            Pause(proc_macro2::Span, bool),
+        }
+
+        // Parse arguments
+        let args_res = raw_args.into_iter().map(|nested_meta| match nested_meta {
+            syn::NestedMeta::Meta(ref meta) => {
+                enum ArgName {
+                    PrefixEnter,
+                    PrefixExit,
+                    Enable,
+                    Disable,
+                    Pause,
+                }
+
+                let ident = meta.name();
+                let arg_name = match ident.to_string().as_str() {
+                    "prefix_enter" => ArgName::PrefixEnter,
+                    "prefix_exit"  => ArgName::PrefixExit,
+                    "enable"       => ArgName::Enable,
+                    "disable"      => ArgName::Disable,
+                    "pause"        => ArgName::Pause,
+                    _ => return Err(vec![syn::parse::Error::new(
+                        ident.span(),
+                        format_args!("unknown attribute argument `{}`", ident),
+                    )]),
+                };
+
+                let prefix_enter_type_error = || vec![
+                    syn::parse::Error::new(ident.span(), "`prefix_enter` requires a string value")
+                ];
+                let prefix_exit_type_error = || vec![
+                    syn::parse::Error::new(ident.span(), "`prefix_exit` requires a string value")
+                ];
+                let enable_type_error = || vec![
+                    syn::parse::Error::new(ident.span(), "`enable` requires a list of meta words")
+                ];
+                let disable_type_error = || vec![
+                    syn::parse::Error::new(ident.span(), "`disable` requires a list of meta words")
+                ];
+                let pause_type_error = || vec![
+                    syn::parse::Error::new(ident.span(), "`pause` must be a meta word")
+                ];
+
+                match *meta {
+                    syn::Meta::Word(_) => match arg_name {
+                        ArgName::Pause => Ok(Arg::Pause(meta.span(), true)),
+
+                        ArgName::PrefixEnter => Err(prefix_enter_type_error()),
+                        ArgName::PrefixExit  => Err(prefix_exit_type_error()),
+                        ArgName::Enable      => Err(enable_type_error()),
+                        ArgName::Disable     => Err(disable_type_error()),
+                    },
+                    syn::Meta::List(syn::MetaList { ref nested, .. }) => match arg_name {
+                        ArgName::Enable => {
+                            let mut idents = HashSet::new();
+                            let mut other_nested_meta_errors = Vec::new();
+
+                            nested.iter().for_each(|nested_meta| match *nested_meta {
+                                syn::NestedMeta::Meta(syn::Meta::Word(ref word)) => {
+                                    idents.insert(word.clone());
+                                },
+                                _ => other_nested_meta_errors.push(syn::parse::Error::new(
+                                    nested_meta.span(),
+                                    "`enable` must contain words only",
+                                )),
+                            });
+
+                            if other_nested_meta_errors.is_empty() {
+                                Ok(Arg::Enable(meta.span(), idents))
+                            } else {
+                                Err(other_nested_meta_errors)
+                            }
+                        },
+                        ArgName::Disable => {
+                            let mut idents = HashSet::new();
+                            let mut other_nested_meta_errors = Vec::new();
+
+                            nested.iter().for_each(|nested_meta| match *nested_meta {
+                                syn::NestedMeta::Meta(syn::Meta::Word(ref word)) => {
+                                    idents.insert(word.clone());
+                                },
+                                _ => other_nested_meta_errors.push(syn::parse::Error::new(
+                                    nested_meta.span(),
+                                    "`disable` must contain words only",
+                                )),
+                            });
+
+                            if other_nested_meta_errors.is_empty() {
+                                Ok(Arg::Disable(meta.span(), idents))
+                            } else {
+                                Err(other_nested_meta_errors)
+                            }
+                        },
+
+                        ArgName::PrefixEnter => Err(prefix_enter_type_error()),
+                        ArgName::PrefixExit  => Err(prefix_exit_type_error()),
+                        ArgName::Pause       => Err(pause_type_error()),
+                    },
+                    syn::Meta::NameValue(syn::MetaNameValue { ref lit, .. }) => match arg_name {
+                        ArgName::PrefixEnter => match *lit {
+                            syn::Lit::Str(ref lit_str) => {
+                                Ok(Arg::PrefixEnter(meta.span(), lit_str.value()))
+                            },
+                            _ => Err(vec![syn::parse::Error::new(
+                                lit.span(),
+                                "`prefix_enter` must have a string value",
+                            )]),
+                        },
+                        ArgName::PrefixExit => match *lit {
+                            syn::Lit::Str(ref lit_str) => {
+                                Ok(Arg::PrefixExit(meta.span(), lit_str.value()))
+                            },
+                            _ => Err(vec![syn::parse::Error::new(
+                                lit.span(),
+                                "`prefix_exit` must have a string value",
+                            )]),
+                        },
+
+                        ArgName::Enable  => Err(enable_type_error()),
+                        ArgName::Disable => Err(disable_type_error()),
+                        ArgName::Pause   => Err(pause_type_error()),
+                    },
+                }
+            },
+            syn::NestedMeta::Literal(_) => {
+                Err(vec![syn::parse::Error::new(nested_meta.span(), "literal attribute not allowed")])
+            },
+        });
+
+        let mut prefix_enter_args = vec![];
+        let mut prefix_exit_args = vec![];
+        let mut enable_args = vec![];
+        let mut disable_args = vec![];
+        let mut pause_args = vec![];
+        let mut errors = vec![];
+
+        // Group arguments of the same type and errors
+        for arg_res in args_res {
+            match arg_res {
+                Ok(arg) => match arg {
+                    Arg::PrefixEnter(span, s)  => prefix_enter_args.push((span, s)),
+                    Arg::PrefixExit(span, s)   => prefix_exit_args.push((span, s)),
+                    Arg::Enable(span, idents)  => enable_args.push((span, idents)),
+                    Arg::Disable(span, idents) => disable_args.push((span, idents)),
+                    Arg::Pause(span, b)        => pause_args.push((span, b)),
+                },
+                Err(es) => errors.extend(es),
+            }
+        }
+
+        // Report duplicates
+        if prefix_enter_args.len() >= 2 {
+            errors.extend(prefix_enter_args.iter().map(|(span, _)| {
+                syn::parse::Error::new(*span, "duplicate `prefix_enter`")
+            }));
+        }
+        if prefix_exit_args.len() >= 2 {
+            errors.extend(prefix_exit_args.iter().map(|(span, _)| {
+                syn::parse::Error::new(*span, "duplicate `prefix_exit`")
+            }));
+        }
+        if enable_args.len() >= 2 {
+            errors.extend(enable_args.iter().map(|(span, _)| {
+                syn::parse::Error::new(*span, "duplicate `enable`")
+            }));
+        }
+        if disable_args.len() >= 2 {
+            errors.extend(disable_args.iter().map(|(span, _)| {
+                syn::parse::Error::new(*span, "duplicate `disable`")
+            }));
+        }
+        if pause_args.len() >= 2 {
+            errors.extend(pause_args.iter().map(|(span, _)| {
+                syn::parse::Error::new(*span, "duplicate `pause`")
+            }));
+        }
+
+        // Report the presence of mutually exclusive arguments
+        if enable_args.len() == 1 && disable_args.len() == 1 {
+            errors.push(syn::parse::Error::new(
+                enable_args[0].0,
+                "cannot have both `enable` and `disable`",
+            ));
+            errors.push(syn::parse::Error::new(
+                disable_args[0].0,
+                "cannot have both `enable` and `disable`",
+            ));
+        }
+
+        if errors.is_empty() {
+            macro_rules! first_no_span {
+                ($iterable:expr) => { $iterable.into_iter().next().map(|(_, elem)| elem) };
+            }
+
+            let prefix_enter = first_no_span!(prefix_enter_args)
+                .unwrap_or_else(|| DEFAULT_PREFIX_ENTER.to_owned());
+            let prefix_exit = first_no_span!(prefix_exit_args)
+                .unwrap_or_else(|| DEFAULT_PREFIX_EXIT.to_owned());
+            let filter = match (first_no_span!(enable_args), first_no_span!(disable_args)) {
+                (None, None) => Filter::None,
+                (Some(idents), None) => Filter::Enable(idents),
+                (None, Some(idents)) => Filter::Disable(idents),
+                (Some(_), Some(_)) => unreachable!(),
+            };
+            let pause = first_no_span!(pause_args)
+                .unwrap_or(DEFAULT_PAUSE);
+
+            Ok(Self { prefix_enter, prefix_exit, filter, pause })
+        } else {
+            Err(errors)
+        }
+    }
+}
